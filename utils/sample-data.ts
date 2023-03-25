f622fd80e459bc1fd4d5f3eaed585c9686501ddb
loc <htanloc994@gmail.com>
2023-03-25 16:29:52 +0700

    change ui

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -2,8 +2,8 @@ import { User } from '../interfaces'
 
 /** Dummy user data. */
 export const sampleUserData: User[] = [
-  { id: 101, name: 'Alice' },
-  { id: 102, name: 'Bob' },
-  { id: 103, name: 'Caroline' },
-  { id: 104, name: 'Dave' },
+  { id: "The-following-is-an-example-of-the", name: 'Alice' },
+  { id: "The-following-is-example-of-the", name: 'Bob' },
+  { id: "following-is-an-example-the", name: 'Caroline' },
+  { id: "The-following-i-example-of-the", name: 'Dave' },
 ]
